29f3e18a92064bf73a06da83a4b8ca769ed8c8df
Rishabh Gupta <rg6802843@gmail.com>
2025-11-11 09:54:34 +0530

    app ,scaling file and templates have been added

---
+++ app.py
@@ -0,0 +1,38 @@
+import pickle
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+
+# load the model
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scaler = pickle.load(open('scaling.pkl', 'rb'))
+
+@app.route('/')
+
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+
+def predict_api():
+    new_data=scaler.transform(input_data)
+    print("Scaled Data:",new_data)
+    output=regmodel.predict(new_data)
+    print("prediction:",output[0])
+    return jsonify({'prediction': float(output[0])})
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)
+    return render_template("home.html", prediction_text="The predicted value is {}".format(output))
+
+
+        
+    
+if __name__=="__main__":
+    app.run(debug=True)
+    
